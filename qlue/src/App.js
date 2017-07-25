ce456824241b5f4fad70a507034c41b97d122c47
stedyyulius <stedyyulius@gmail.com>
2017-07-25 22:53:10 +0700

    waze feature done

--- qlue/src/App.js
+++ qlue/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Menubar from './components/Menubar.js'
 import QlueMap from './components/Map.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Menubar />
           <QlueMap />
       </div>
     );
--- qlue/src/components/Map.js
+++ qlue/src/components/Map.js
@@ -1,38 +1,83 @@
 import React, {Component} from 'react'
+import { divIcon, icon } from 'leaflet';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import DivIcon from 'react-leaflet-div-icon';
 import axios from 'axios'
 
-const position = [51.505, -0.09];
+const Jakarta = [-6.2, 106.8689];
+var Terminal = icon({
+    iconUrl: 'http://www.qlue.co.id/vacancy/svc/icon-marker.png',
+    iconSize: [30, 30],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
+
+var Traffic = icon({
+    iconUrl: 'https://s-media-cache-ak0.pinimg.com/originals/cd/44/f9/cd44f96b2ac5b7361bf53c77e2fd5d3d.png',
+    iconSize: [30, 30],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
+
 
 class QlueMap extends Component {
   constructor(props){
     super(props)
     this.state={
-      markers:{}
+      markers:{},
+      waze: {}
     }
   }
   
   render(){
-    return(
-        <Map center={position} zoom={0}>
+    return(<div>
+        <button onClick={()=>this.tes()}>Tes</button>
+        <Map center={Jakarta} zoom={12}>
           <TileLayer
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          /><button type="button" onClick={()=>this.tes()}>tes</button>
+          />
         {(this.state.markers.length > 0)
          ?this.state.markers.map((m,index) => (
            <Marker
-            key={index} 
-            position={[+m.lat,+m.lng]}>
+             key={index} 
+             position={[+m.lat,+m.lng]}
+             icon={Terminal}>
             <Popup>
-              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+              <span>
+                <b>{m.name}</b>
+                <br />
+                {m.address}
+              </span>
             </Popup>
-           </Marker> 
+          </Marker>          
         ))
         :<div><img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" 
               alt="loading"/></div>
       }
-        </Map>
+        {(this.state.waze.length > 0)
+         ?this.state.waze.map((w,index) => (
+           <Marker
+             key={index} 
+             position={[w.location.y,w.location.x]}
+             >
+            <Popup>
+              <span>
+                <b>{w.street}</b>
+                <br />
+                {w.subtype}
+              </span>
+            </Popup>
+          </Marker>          
+        ))
+        :<div><img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" 
+              alt="loading"/></div>
+      }
+    </Map></div>
     )
   }
   
@@ -42,9 +87,17 @@ class QlueMap extends Component {
   
   componentWillMount(){
     axios.get(`http://www.qlue.co.id/vacancy/svc/getDataExample.php`)
-    .then(markers=>{
+    .then(terminals=>{
+      console.log(terminals.data);
       this.setState({
-        markers: markers.data
+        markers: terminals.data
+      })
+    })
+    axios.get(`http://waze.qlue.id/jakarta/update/0atxn84I3hx2WmNm5ifPDZkJaLERZD9A.json`)
+    .then(waze=>{
+      console.log(waze.data.alerts[0].location.x);
+      this.setState({
+        waze: waze.data.alerts
       })
     })
   }
