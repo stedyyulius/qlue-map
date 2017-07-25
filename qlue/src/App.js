7bd294213712235570d56fa51d6454343df6fd7c
stedyyulius <stedyyulius@gmail.com>
2017-07-25 19:34:13 +0700

    map

---
+++ qlue/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Map from './components/Map.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Map />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ qlue/src/components/Map.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+
+
+class Map extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      
+    }
+  }
+  render(){
+    return(
+      <div>
+        <Map center={position} zoom={13}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            </Popup>
+          </Marker>
+        </Map>
+      </div>
+    )
+  }
+}
+
+export default Map
